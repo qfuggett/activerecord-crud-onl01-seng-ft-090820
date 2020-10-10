747f236b615ffc028be6893e6b9852c864ddc6f1
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-10 03:59:01 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,8 +29,7 @@ def can_be_created_in_a_block(args = {})
   # release_date == 1990
   
   Movie.create do |m|
-    m.create(title
-    m.release_date
+    m.create(title, release_date)
   end
 end
 
